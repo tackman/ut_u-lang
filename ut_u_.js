e29027b1fcf59b157eba43e6d78dc04e22ac4c93
tackman <presitac@gmail.com>
2012-05-03 14:49:45 +0900

    動作したっぽい

--- ut_u_.js
+++ ut_u_.js
@@ -5,6 +5,7 @@ var g_itr;
 var g_tokens = [];
 var g_tk_state = 'start';
 
+
 function tokenize(str){
     var sp = str.split(' ');
     for( i in sp ){
@@ -52,12 +53,129 @@ function output(arg){
     console.log(arg);
 };
 
-// entry point
+// interpreter area
+var g_ptr;
+var g_pc;
+function run(){
+    var added;
+    var decced;
+    var depth;
+    var i;
+    var value;
+    g_ptr = 0;
+    g_pc = 0;
+    while(g_pc < g_tokens.length){
+	var token = g_tokens[g_pc];
+	if(token === 'あうー'){
+	    ;  // nop
+	}else if(token === 'イエイ'){
+	    g_ptr++;
+	}else if(token === 'おとく'){
+	    g_ptr--;
+	}else if(token === 'うっうー'){
+	    for( i = g_ptr; ; ++i){
+		value = g_tokens[i];
+		added = half_add(value);
+		g_tokens[i] = added;
+		if(added !== 'あうー')
+		    break;
+	    }
+	}else if(token === 'ううー'){
+	    for(i = g_ptr; ; --i){
+		value = g_tokens[i];
+		decced = half_dec(value);
+		g_tokens[i] = decced;
+		if(decced !== 'かなーって')
+		    break;
+	    }
+	}else if(token === 'ハイ、ターッチ'){
+	    stdout(g_tokens[g_ptr]);
+	}else if(token === 'かもー'){
+	    depth = 0;
+	    ++g_pc;
+	    if(g_tokens[g_ptr] === 'あうー'){
+		for(; ; ++g_pc){
+		    if(g_tokens[g_pc] === 'かなーって' && depth === 0)
+			break;
+		    if(g_tokens[g_pc] === 'かもー')
+			depth++;
+		    else if(g_tokens[g_pc] === 'かなーって')
+		        depth--;
+		}
+	    }
+	}else if(token === 'かなーって'){
+	    depth = 0;
+	    if(g_tokens[g_ptr] !== 'あうー'){
+		--g_pc;
+		for(;;--g_pc){
+		    if(g_tokens[g_pc] === 'かもー' && depth === 0)
+			break;
+		    if(g_tokens[g_pc] === 'かなーって')
+			depth++;
+		    else if(g_tokens[g_pc] === 'かもー')
+		        depth--;
+		}
+	    }else{
 
-var _div = tokenize('あいうえう うっうー ううー あうー かなーって かもー');
-output(_div);
-for( _s in _div){
-    if(_div[_s] === 'うっうー'){
-	output(_div[_s]);
+	    }
+	}
+
+	++g_pc;
     }
 }
+
+function stdout(s){
+    // switches with env
+    // e.g. node.js or on-Browser script etc...
+    console.log(s);
+}
+
+function half_add( value){
+    var ret;
+    if(value === 'あうー')
+	ret = 'うっうー';
+    else if(value === 'うっうー')
+        ret = 'ううー';
+    else if(value === 'ううー')
+        ret = 'イエイ';
+    else if(value === 'イエイ')
+        ret = 'おとく';
+    else if(value === 'おとく')
+        ret = 'ハイ、ターッチ';
+    else if(value === 'ハイ、ターッチ')
+        ret = 'かもー';
+    else if(value === 'かもー')
+        ret = 'かなーって';
+    else if(value === 'かなーって')
+        ret = 'あうー';
+    return ret;
+}
+
+function half_dec( value){
+   var ret;
+    if(value === 'あうー')
+	ret = 'かもー';
+    else if(value === 'うっうー')
+        ret = 'あうー';
+    else if(value === 'ううー')
+        ret = 'うっうー';
+    else if(value === 'イエイ')
+        ret = 'ううー';
+    else if(value === 'おとく')
+        ret = 'イエイ';
+    else if(value === 'ハイ、ターッチ')
+        ret = 'おとく';
+    else if(value === 'かもー')
+        ret = 'ハイ、ターッチ';
+    else if(value === 'かなーって')
+        ret = 'ハイ、ターッチ';
+    return ret;
+
+}
+
+// entry point
+
+var _div = tokenize('あうー うっうー かもー ハイ、ターッチ  うっうー かなーって');
+run();
+
+
